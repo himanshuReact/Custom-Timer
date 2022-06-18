5b001acb7789e1b93420d7fa3d70cb7f649af2bf
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-06-19 00:06:43 +0530

    updated Custom timer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useTimer = () => {
-  const [seconds, setSeconds] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds(seconds + 1);
+const useCustomTimer = (maxTime) => {
+  const [isRunning, setRunning] = useState(false);
+
+  const [seconds, setSeconds] = useState(maxTime);
+  let intervalIdRef = useRef(null);
+
+  const handleStart = useCallback(() => {
+    setRunning(true);
+    console.log("start clicked");
+    intervalIdRef.current = setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
     }, 1000);
-    return () => clearInterval(interval);
-  });
-  return seconds;
+  }, [setSeconds, setRunning]);
+  const handleStop = useCallback(() => {
+    console.log("stop clicked");
+    setRunning(false);
+    clearInterval(intervalIdRef.current);
+    setSeconds(maxTime);
+  }, [setSeconds, setRunning, maxTime]);
+
+  useEffect(() => {
+    if (seconds < 1) {
+      // setRunning(false);
+      // clearInterval(intervalIdRef.current);
+      // setSeconds(maxTime);
+      handleStop();
+    }
+  }, [seconds, handleStop]);
+
+  useEffect(() => {
+    // clear interval when component unmount happens
+    return () => intervalIdRef && clearInterval(intervalIdRef.current);
+  }, []);
+
+  return {
+    isRunning,
+    handleStart,
+    handleStop,
+    seconds
+  };
 };
 
 const Timer = () => {
-  const seconds = useTimer();
+  const { isRunning, handleStart, handleStop, seconds } = useCustomTimer(5);
+
   return (
     <div className="App">
       <div>Seconds: {seconds} </div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>End</button>
     </div>
   );
 };
